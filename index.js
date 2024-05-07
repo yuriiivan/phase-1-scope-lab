2fac5a35cf33005055bfe71f4597402e4a80c5c4
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-07 09:44:40 -0400

    Pass tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const lestFavoriteCustomer = 'John'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    lestFavoriteCustomer = 'bob'
+}
